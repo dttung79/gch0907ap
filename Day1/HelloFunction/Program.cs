13ea2c0dfa8f1b45fc14a58828b00a04f5055107
dttung79 <dttung@gmail.com>
2022-07-14 11:31:29 +0700

    1st time submit

---
+++ Day1/HelloFunction/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HelloFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1, 3, 4, 9, 19, 33, 75};
+            // Console.WriteLine("Enter x: ");
+            // int x = Convert.ToInt32(Console.ReadLine());
+            // int position = Search(a, x);
+            // if (position == -1) Console.WriteLine("Not found");
+            // else Console.WriteLine("Found at " + position);
+
+            // Console.WriteLine("Sum of array: " + Sum(a));
+            // Ask user to choose even or odd then call SearchEvenOdd function
+            Console.WriteLine("Which type of sum/count do you want?");
+            Console.WriteLine("1. Even");
+            Console.WriteLine("2. Odd");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            const int EVEN = 1;
+            const int ODD = 2;
+            // if (choice == 1) Console.WriteLine("Sum of even numbers: " + SumEvenOrOdd(a, true));
+            // else Console.WriteLine("Sum of odd numbers: " + SumEvenOrOdd(a, false));
+            Console.WriteLine("Sum = " + SumEvenOrOdd(a, choice == EVEN));
+            // Count of even or odd numbers in array
+            Console.WriteLine("Count: " + CountEvenOrOdd(a, choice == EVEN));
+        }
+        static int Search(int[] arr, int x)
+        {
+            // find x in arr, return found index or -1 if not found
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (x == arr[i]) return i;
+            }
+            return -1;
+        }
+        // write a function to calculate sum of array
+        static int Sum(int[] a)
+        {
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+                s = s + a[i];
+            
+            return s;
+        }
+
+        // write a function to calculate sum of even or odd numbers in array
+        static int SumEvenOrOdd(int[] a, bool even)
+        {
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] % 2 == 0) == even)
+                    s += a[i];          
+            }
+            return s;
+        }
+
+        // write a function to count number of even or odd numbers in array
+        static int CountEvenOrOdd(int[] a, bool even)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] % 2 == 0) == even)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Day2/AutoVendingMachine/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace AutoVendingMachine
+{
+    class Program
+    {
+        const int COKE = 1;
+        const int WATER = 2;
+        const int TEA = 3;
+        const double COKE_PRICE = 2.0;
+        const double WATER_PRICE = 1.0;
+        const double TEA_PRICE = 1.5;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PrintDrinkMenu();
+                int drinkChoice = GetDrinkChoice();
+                int nDrinks = GetNumberOfDrinks();
+                double payment = GetPayment(drinkChoice, nDrinks);
+                PayBill(payment);
+            }
+        }
+        static void PrintDrinkMenu()
+        {
+            Console.WriteLine("1. Coke ($2.0)");
+            Console.WriteLine("2. Water ($1.0)");
+            Console.WriteLine("3. Tea ($1.5)");
+        }
+        static int GetDrinkChoice()
+        {
+            Console.Write("Choose your drink: ");
+            int drinkChoice = 0;
+            while (!ValidChoice(drinkChoice))
+            {
+                drinkChoice = int.Parse(Console.ReadLine());
+                if (!ValidChoice(drinkChoice))
+                {
+                    Console.Write("Invalid choice. Please choose again: ");
+                }
+            }
+            return drinkChoice;
+        }
+        static bool ValidChoice(int drinkChoice)
+        {
+            if (drinkChoice < 1 || drinkChoice > 3) return false;
+            
+            return true;
+        }
+
+        static int GetNumberOfDrinks()
+        {
+            // HW: Validate number of drinks in range 1-5
+            Console.Write("Number of drinks: ");
+            int nDrinks = int.Parse(Console.ReadLine());
+            return nDrinks;
+        }
+        static double GetPayment(int drinkChoice, int nDrinks)
+        {
+            double payment = 0;
+            switch (drinkChoice)
+            {
+                case COKE:
+                    payment = COKE_PRICE * nDrinks;
+                    break;
+                case WATER:
+                    payment = WATER_PRICE * nDrinks;
+                    break;
+                case TEA:
+                    payment = TEA_PRICE * nDrinks;
+                    break;
+                default:
+                    break;
+            }
+            return payment;
+        }
+        static void PayBill(double payment)
+        {
+            Console.WriteLine("Payment: $" + payment);
+            bool notEnough = true;
+            while (notEnough)
+            {
+                double coin = GetCoin();
+                payment = CheckPayment(coin, payment);
+                notEnough = payment > 0;
+            }
+        }
+        static double GetCoin()
+        {
+            bool invalid = true;
+            double coin = 0;
+            while (invalid)
+            {
+                Console.Write("Insert coin: ");
+                coin = double.Parse(Console.ReadLine());
+                invalid = coin != 1 && coin != 2 && coin != 5 && coin != 10;
+                if (invalid) Console.WriteLine("Invalid coin. Please insert again.");
+            }
+            return coin;
+        }
+        static double CheckPayment(double coin, double payment)
+        {
+            double change = coin - payment;
+            if (change == 0)
+            {
+                Console.WriteLine("No change");
+            }
+            else if (change > 0)
+            {
+                Console.WriteLine("Change: $" + change);
+            }
+            else
+            {
+                Console.WriteLine("Payment: $" + (-change));
+            }
+            return -change;
+        }
+    }
+}
